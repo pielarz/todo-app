b79989ff96bfd449f624205dc19513be107f33d0
Michał Pielarz <pielarz.michal@gmail.com>
2020-08-20 04:08:53 +0200

    TaskController with Logger test

---
+++ src/main/java/io/github/pielarz/todoapp/controller/TaskController.java
@@ -0,0 +1,16 @@
+package io.github.pielarz.todoapp.controller;
+
+import io.github.pielarz.todoapp.repository.TaskRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+
+@RepositoryRestController
+public class TaskController {
+    public static final Logger logger = LoggerFactory.getLogger(TaskController.class);
+    private final TaskRepository repository;
+
+    public TaskController(TaskRepository repository) {
+        this.repository = repository;
+    }
+}
