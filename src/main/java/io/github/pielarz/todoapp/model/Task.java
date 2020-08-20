fa8fc9a66bb69b5a7566582c0dcfa84f37a1b444
Michał Pielarz <pielarz.michal@gmail.com>
2020-08-20 04:04:53 +0200

    entity Task

--- src/main/java/io/github/pielarz/todoapp/model/Task.java
+++ src/main/java/io/github/pielarz/todoapp/model/Task.java
@@ -1,19 +1,23 @@
 package io.github.pielarz.todoapp.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "tasks")
 public class Task {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @NotBlank(message = "Task's description must not be empty")
     private String description;
 
     private boolean done;
+
+    public Task() {
+    }
 
     public int getId() {
         return id;
