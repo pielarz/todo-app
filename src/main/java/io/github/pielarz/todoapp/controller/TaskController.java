1f10fc8e741bd1ac08c9cc062644df4b506bcf0f
Michał Pielarz <pielarz.michal@gmail.com>
2020-08-22 21:40:42 +0200

    changed mapping annotation from version < 2.5, added POST method

--- src/main/java/io/github/pielarz/todoapp/controller/TaskController.java
+++ src/main/java/io/github/pielarz/todoapp/controller/TaskController.java
@@ -6,13 +6,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 
-@RestController
+@Controller
 public class TaskController {
     public static final Logger logger = LoggerFactory.getLogger(TaskController.class);
     private final TaskRepository repository;
@@ -21,15 +23,29 @@ public class TaskController {
         this.repository = repository;
     }
 
-    @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
+    @RequestMapping(value = "/tasks", params = {"!sort", "!page", "!size"}, method = RequestMethod.GET)
     ResponseEntity<List<Task>> readAllTasks() {
         logger.warn("Exposing all the tasks!");
         return ResponseEntity.ok(repository.findAll());
     }
 
-    @GetMapping("/tasks")
+    @RequestMapping(value = "/tasks", method = RequestMethod.GET)
     ResponseEntity<List<Task>> readAllTasks(Pageable pageable) {
         logger.info("Custom pageable.");
         return ResponseEntity.ok(repository.findAll(pageable).getContent());
+    }
+
+    @RequestMapping(value = "/tasks/{id}", method = RequestMethod.GET)
+    ResponseEntity<Task> readById(@PathVariable Integer id, @RequestBody Task task){
+        if(!repository.existsById(id)){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(repository.findById(id).get());
+    }
+
+    @RequestMapping(value = "/tasks", method = RequestMethod.POST)
+    ResponseEntity<Task> createTask(@RequestBody Task task) throws URISyntaxException {
+        repository.save(task);
+        return ResponseEntity.created(new URI("localhost:8080/tasks")).build();
     }
 }
