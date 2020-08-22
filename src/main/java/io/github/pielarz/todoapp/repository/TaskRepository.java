c7a1a28b61b27c7475cd8b375cfe2baf9972f5eb
Michał Pielarz <pielarz.michal@gmail.com>
2020-08-22 20:20:11 +0200

    removing unnessesairy methods

--- src/main/java/io/github/pielarz/todoapp/repository/TaskRepository.java
+++ src/main/java/io/github/pielarz/todoapp/repository/TaskRepository.java
@@ -12,20 +12,7 @@ import java.util.List;
 @RepositoryRestResource
 public interface TaskRepository extends JpaRepository<Task, Integer> {
 
-
-    @Override
-    @RestResource(exported = false)
-    void deleteById(Integer integer);
-
-
-    @Override
-    @RestResource(exported = false)
-    void delete(Task entity);
-
     @RestResource(path = "done", rel = "done")
     List<Task> findByDone(@Param("state") boolean done);
 
-    //localhost:8080/tasks/search/done&desc?state=false&state2=testing
-    @RestResource(path = "done&desc", rel = "done&desc")
-    List<Task> findByDoneAndDescription(@Param("state") boolean done, @Param("state2") String description);
 }
