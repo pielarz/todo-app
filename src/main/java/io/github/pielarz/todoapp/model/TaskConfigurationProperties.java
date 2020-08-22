3ffeef316c4d8a145be2afcc646441a643eee218
Michał Pielarz <pielarz.michal@gmail.com>
2020-08-23 00:16:14 +0200

    New configuration property

--- src/main/java/io/github/pielarz/todoapp/model/TaskConfigurationProperties.java
+++ src/main/java/io/github/pielarz/todoapp/model/TaskConfigurationProperties.java
@@ -1,4 +1,18 @@
 package io.github.pielarz.todoapp.model;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties("task")
 public class TaskConfigurationProperties {
+    private boolean allowMultipleTasksFromTemplate;
+
+    public boolean isAllowMultipleTasksFromTemplate() {
+        return allowMultipleTasksFromTemplate;
+    }
+
+    public void setAllowMultipleTasksFromTemplate(boolean allowMultipleTasksFromTemplate) {
+        this.allowMultipleTasksFromTemplate = allowMultipleTasksFromTemplate;
+    }
 }
