0f7536e0b84494673b021a7be539a8ba775ccf4d
Michał Pielarz <pielarz.michal@gmail.com>
2020-08-22 20:12:50 +0200

    get method in task controller

--- src/main/java/io/github/pielarz/todoapp/controller/TaskController.java
+++ src/main/java/io/github/pielarz/todoapp/controller/TaskController.java
@@ -5,14 +5,14 @@ import io.github.pielarz.todoapp.repository.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 
-@RepositoryRestController
+@RestController
 public class TaskController {
     public static final Logger logger = LoggerFactory.getLogger(TaskController.class);
     private final TaskRepository repository;
