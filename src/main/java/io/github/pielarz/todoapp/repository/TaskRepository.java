701cc063e846e0d0d5b5e560ecf9b019597c167b
Michał Pielarz <pielarz.michal@gmail.com>
2020-08-24 19:54:37 +0200

    ... -> super-class

--- src/main/java/io/github/pielarz/todoapp/repository/TaskRepository.java
+++ src/main/java/io/github/pielarz/todoapp/repository/TaskRepository.java
@@ -1,18 +1,31 @@
 package io.github.pielarz.todoapp.repository;
 
 import io.github.pielarz.todoapp.model.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @RepositoryRestResource
-public interface TaskRepository extends JpaRepository<Task, Integer> {
+public interface TaskRepository {
 
-    @RestResource(path = "done", rel = "done")
     List<Task> findByDone(@Param("state") boolean done);
 
+    boolean existsById(Integer id);
+
+    List<Task> findAll();
+
+    Page<Task> findAll(Pageable page);
+
+    Task save(Task entity);
+
+    Optional<Task> findById(Integer id);
+
+    void deleteById(Integer id);
 }
