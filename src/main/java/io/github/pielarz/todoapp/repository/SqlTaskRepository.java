aca8aa7cfae4b2cb5aa1637d0b00b4e750ca38f3
Michał Pielarz <pielarz.michal@gmail.com>
2020-08-24 19:53:40 +0200

    native query

--- src/main/java/io/github/pielarz/todoapp/repository/SqlTaskRepository.java
+++ src/main/java/io/github/pielarz/todoapp/repository/SqlTaskRepository.java
@@ -1,4 +1,15 @@
 package io.github.pielarz.todoapp.repository;
 
-public interface SqlTaskRepository {
+import io.github.pielarz.todoapp.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SqlTaskRepository extends TaskRepository, JpaRepository<Task, Integer> {
+
+    @Override
+    @Query(nativeQuery = true, value="select count(*) > 0 from tasks where id=:id")
+    boolean existsById(Integer id);
 }
