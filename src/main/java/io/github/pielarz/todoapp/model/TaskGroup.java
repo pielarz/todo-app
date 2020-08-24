458ce39ff39427701b6489802546ea1eba58b6d4
Michał Pielarz <pielarz.michal@gmail.com>
2020-08-24 23:49:41 +0200

    TaskGroup entity

---
+++ src/main/java/io/github/pielarz/todoapp/model/TaskGroup.java
@@ -0,0 +1,45 @@
+package io.github.pielarz.todoapp.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "task_groups")
+public class TaskGroup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotBlank(message = "Task group's description must not be empty")
+    private String description;
+    private boolean done;
+    @Embedded
+    private Audit audit = new Audit();
+
+    public TaskGroup() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(final boolean done) {
+        this.done = done;
+    }
+}
