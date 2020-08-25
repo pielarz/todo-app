e2135cbcd7863d956dfe49d3904bf621f3f7a20a
Michał Pielarz <pielarz.michal@gmail.com>
2020-08-25 22:46:33 +0200

    Project repo

--- src/main/java/io/github/pielarz/todoapp/repository/ProjectRepository.java
+++ src/main/java/io/github/pielarz/todoapp/repository/ProjectRepository.java
@@ -1,4 +1,15 @@
 package io.github.pielarz.todoapp.repository;
 
+import io.github.pielarz.todoapp.model.Project;
+
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ProjectRepository {
+    List<Project> findAll();
+
+    Optional<Project> findById(Integer id);
+
+    Project save(Project entity);
 }
