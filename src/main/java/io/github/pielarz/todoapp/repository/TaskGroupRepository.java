4ab402f6ff932a649a081e9c59fe080b3cd221c1
Michał Pielarz <pielarz.michal@gmail.com>
2020-08-25 22:46:09 +0200

    native query

--- src/main/java/io/github/pielarz/todoapp/repository/TaskGroupRepository.java
+++ src/main/java/io/github/pielarz/todoapp/repository/TaskGroupRepository.java
@@ -1,4 +1,17 @@
 package io.github.pielarz.todoapp.repository;
 
+import io.github.pielarz.todoapp.model.TaskGroup;
+
+import java.util.List;
+import java.util.Optional;
+
+
 public interface TaskGroupRepository {
+    List<TaskGroup> findAll();
+
+    Optional<TaskGroup> findById(Integer id);
+
+    TaskGroup save(TaskGroup entity);
+
+    boolean existsByDoneIsFalseAndProject_Id(Integer projectId);
 }
