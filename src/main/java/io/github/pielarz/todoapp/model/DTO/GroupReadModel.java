bdaad1d3ca81c054285e93e3c71e35940cfa866f
Michał Pielarz <pielarz.michal@gmail.com>
2020-08-30 03:37:22 +0200

    Group read model DTO

--- src/main/java/io/github/pielarz/todoapp/model/DTO/GroupReadModel.java
+++ src/main/java/io/github/pielarz/todoapp/model/DTO/GroupReadModel.java
@@ -1,4 +1,49 @@
-package io.github.pielarz.todoapp.model.projection;
+package io.github.pielarz.todoapp.model.DTO;
+
+import io.github.pielarz.todoapp.model.Task;
+import io.github.pielarz.todoapp.model.TaskGroup;
+
+import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class GroupReadModel {
+    private String description;
+    private LocalDateTime deadline; //Deadline from latest task in group
+    private Set<GroupTaskReadModel> tasks;
+
+    public GroupReadModel(TaskGroup source) {
+        description = source.getDescription();
+        source.getTasks().stream()
+                .map(Task::getDeadline)
+                .max(LocalDateTime::compareTo)
+                .ifPresent(date -> deadline = date);
+
+        tasks = source.getTasks().stream().map(GroupTaskReadModel::new).collect(Collectors.toSet());
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDateTime deadline) {
+        this.deadline = deadline;
+    }
+
+    public Set<GroupTaskReadModel> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(Set<GroupTaskReadModel> tasks) {
+        this.tasks = tasks;
+    }
 }
