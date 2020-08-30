d730a2ee6a8a7ee0d8443f69f1fce14b716117fd
Michał Pielarz <pielarz.michal@gmail.com>
2020-08-30 03:26:16 +0200

    Task write model dto

--- src/main/java/io/github/pielarz/todoapp/model/DTO/GroupTaskWriteModel.java
+++ src/main/java/io/github/pielarz/todoapp/model/DTO/GroupTaskWriteModel.java
@@ -1,4 +1,30 @@
-package io.github.pielarz.todoapp.model.projection;
+package io.github.pielarz.todoapp.model.DTO;
+
+import io.github.pielarz.todoapp.model.Task;
+
+import java.time.LocalDateTime;
 
 public class GroupTaskWriteModel {
+    private String description;
+    private LocalDateTime deadline;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDateTime deadline) {
+        this.deadline = deadline;
+    }
+
+    public Task toTask(){
+        return new Task(description, deadline);
+    }
 }
