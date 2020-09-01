be5c420d0a0705d0244d8c989e5f3726310ebe6a
Michał Pielarz <pielarz.michal@gmail.com>
2020-09-01 23:47:50 +0200

    another test

--- src/test/java/io/github/pielarz/todoapp/service/ProjectServiceTest.java
+++ src/test/java/io/github/pielarz/todoapp/service/ProjectServiceTest.java
@@ -1,13 +1,14 @@
 package io.github.pielarz.todoapp.service;
 
 import io.github.pielarz.todoapp.config.TaskConfigurationProperties;
+import io.github.pielarz.todoapp.model.TaskGroup;
 import io.github.pielarz.todoapp.repository.ProjectRepository;
 import io.github.pielarz.todoapp.repository.TaskGroupRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyInt;
@@ -80,6 +81,19 @@ class ProjectServiceTest {
 
     }
 
+    @Test
+    @DisplayName("Test should create a new group from project")
+    void createGroup_configurationOk_existingProjects_createAndSavesGroup() {
+        //given
+        var mockRepository = mock(ProjectRepository.class);
+        when(mockRepository.findById(anyInt())).thenReturn(Optional.empty());
+        //and
+        TaskGroupRepository inMemoryGroupRepository = inMemoryGroupRepository();
+        //and
+        TaskConfigurationProperties mockConfig = configurationReturning(true);
+
+    }
+
     private TaskGroupRepository groupRepositoryReturning(boolean result) {
         var mockGroupRepository = mock(TaskGroupRepository.class);
         when(mockGroupRepository.existsByDoneIsFalseAndProject_Id(anyInt())).thenReturn(result);
@@ -93,5 +107,43 @@ class ProjectServiceTest {
         var mockConfig = mock(TaskConfigurationProperties.class);
         when(mockConfig.getTemplate()).thenReturn(mockTemplate);
         return mockConfig;
+    }
+
+    private TaskGroupRepository inMemoryGroupRepository() {
+        return new TaskGroupRepository() {
+
+            private int index = 0;
+            private Map<Integer, TaskGroup> map = new HashMap<>();
+
+            @Override
+            public List<TaskGroup> findAll() {
+                return new ArrayList<>(map.values());
+            }
+
+            @Override
+            public Optional<TaskGroup> findById(Integer id) {
+                return Optional.ofNullable(map.get(id));
+            }
+
+            @Override
+            public TaskGroup save(TaskGroup entity) {
+                if (entity.getId() == 0) {
+                    try {
+                        TaskGroup.class.getDeclaredField("id").set(entity, ++index);
+                    } catch (NoSuchFieldException | IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                map.put(entity.getId(), entity);
+                return entity;
+            }
+
+            @Override
+            public boolean existsByDoneIsFalseAndProject_Id(final Integer projectId) {
+                return map.values().stream()
+                        .filter(group -> !group.isDone())
+                        .anyMatch(group -> group.getProject() != null && group.getProject().getId() == projectId);
+            }
+        };
     }
 }
