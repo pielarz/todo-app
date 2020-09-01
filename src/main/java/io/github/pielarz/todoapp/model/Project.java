378db036894ddf1cc710d1fa170d0fbb78a83d04
Michał Pielarz <pielarz.michal@gmail.com>
2020-09-01 23:32:07 +0200

    added constructor

--- src/main/java/io/github/pielarz/todoapp/model/Project.java
+++ src/main/java/io/github/pielarz/todoapp/model/Project.java
@@ -17,6 +17,8 @@ public class Project {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "project")
     private Set<ProjectStep> steps;
 
+    public Project() {
+    }
 
     public int getId() {
         return id;
